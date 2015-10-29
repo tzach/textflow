ab5643b623eca23786c911f902edd6365b3c1f7e
Tzach Livyatan <tzach@cloudius-systems.com>
2015-10-29 10:55:23 +0200

    upgrade to latest lib release

--- project.clj
+++ project.clj
@@ -1,20 +1,20 @@
 (defproject textflow "0.1.5"
   :description "Online generation of RFC like call flows"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/tzach/textflow"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.5" :exclusions [ring/ring-core]]
-                 [hiccup "1.0.3"]
-                 [com.novemberain/monger "1.5.0"]
-                 [ring/ring-devel "1.1.8"]
-                 [ring/ring-jetty-adapter "1.1.8"]
-                 [ring/ring-json "0.2.0"]
-                 [jayq "2.3.0"]
-                 [com.newrelic.agent.java/newrelic-api "2.17.2"]
-                 [org.clojure/tools.reader "0.7.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.4.0" :exclusions [ring/ring-core]]
+                 [hiccup "1.0.5"]
+                 [com.novemberain/monger "3.0.1"]
+                 [ring/ring-devel "1.4.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [ring/ring-json "0.4.0"]
+                 [jayq "2.5.4"]
+;;                 [com.newrelic.agent.java/newrelic-api "2.17.2"]
+                 [org.clojure/tools.reader "0.10.0"]]
   :min-lein-version "2.0.0"
-  :plugins [[lein-cljsbuild "0.3.0"]]
+  :plugins [[lein-cljsbuild "1.1.0-SNAPSHOT"]]
 ;;  :hooks [leiningen.cljsbuild]
   :cljsbuild {:builds
               [{:source-paths ["src-cljs"],
@@ -41,9 +41,6 @@
   )
 
  
-  ;; todo
-  ;; testing with midjet
-  ;; https://github.com/marick/Midje
 
   ;; usfull commands
   ;; lein trampoline cljsbuild repl-rhino
